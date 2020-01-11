e47b68ffa3cab44bb75a0ede76e9d6fb65be7fc7
Nikhil Raheja <nick170294@gmail.com>
2020-01-11 23:52:58 +0530

    added Array question

---
+++ dataStructures/array/ContainerWithMostWater.java
@@ -0,0 +1,43 @@
+package array;
+
+/*
+ * 	Link: https://leetcode.com/problems/container-with-most-water/
+ * 	Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). 
+ * 	n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, 
+ * 	which together with x-axis forms a container, such that the container contains the most water.
+
+	Note: You may not slant the container and n is at least 2.
+	
+	Example:
+
+	Input: [1,8,6,2,5,4,8,3,7]
+	Output: 49
+ */
+public class ContainerWithMostWater {
+	public static int maxArea(int[] height) {
+		int i = 0;
+		int j = height.length - 1;
+
+		int possibleHeight, possibleWidth, possibleArea;
+		int maxArea = 0;
+		while (i < j) {
+			possibleWidth = j - i;
+			if (height[i] < height[j]) {
+				possibleHeight = height[i++];
+			} else {
+				possibleHeight = height[j--];
+			}
+
+			possibleArea = possibleHeight * possibleWidth;
+			maxArea = possibleArea > maxArea ? possibleArea : maxArea;
+
+		}
+		return maxArea;
+	}
+
+	public static void main(String[] args) {
+
+		int arr[] = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+		System.out.println(maxArea(arr));
+	}
+}
